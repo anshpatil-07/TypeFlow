0376642c7649cbfb0c9a79b6bc21e28d177dfbce
Ansh Alankar Patil <anshapatil07@gmail.com>
2026-06-03 14:14:16 +0530

    fix(completer): fix latency prefill bottleneck and loops

--- TypeFlow/Services/TextInjector.swift
+++ TypeFlow/Services/TextInjector.swift
@@ -20,12 +20,14 @@ class TextInjector {
             // Create KeyDown event
             if let keyDownEvent = CGEvent(keyboardEventSource: source, virtualKey: 0, keyDown: true) {
                 keyDownEvent.keyboardSetUnicodeString(stringLength: 1, unicodeString: &varChar)
+                keyDownEvent.setIntegerValueField(.eventSourceUserData, value: 9999)
                 keyDownEvent.post(tap: .cgSessionEventTap)
             }
             
             // Create KeyUp event
             if let keyUpEvent = CGEvent(keyboardEventSource: source, virtualKey: 0, keyDown: false) {
                 keyUpEvent.keyboardSetUnicodeString(stringLength: 1, unicodeString: &varChar)
+                keyUpEvent.setIntegerValueField(.eventSourceUserData, value: 9999)
                 keyUpEvent.post(tap: .cgSessionEventTap)
             }
         }
@@ -39,9 +41,11 @@ class TextInjector {
         
         for _ in 0..<count {
             if let keyDownEvent = CGEvent(keyboardEventSource: source, virtualKey: 51, keyDown: true) {
+                keyDownEvent.setIntegerValueField(.eventSourceUserData, value: 9999)
                 keyDownEvent.post(tap: .cgSessionEventTap)
             }
             if let keyUpEvent = CGEvent(keyboardEventSource: source, virtualKey: 51, keyDown: false) {
+                keyUpEvent.setIntegerValueField(.eventSourceUserData, value: 9999)
                 keyUpEvent.post(tap: .cgSessionEventTap)
             }
         }
